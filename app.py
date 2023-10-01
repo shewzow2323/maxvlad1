caaebedb31505548518f60a43094b096a76f335c
Кривошеев Максим <m_kriv0sheev@mail.ru>
2023-10-01 13:26:05 +0700

    Добавление

--- app.py
+++ app.py
@@ -2,4 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def start():
-    return 'web-server на Flaskeflask'
+    return '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Кривошеев/Гавра</title>
+</head>
+<body>
+    <header>
+        Кривошеев Гавра, ФБИ-11, 3 курс
+    </header>
+
+    <main> 
+
+    </main>
+
+    <footer>
+        Кривошеев Гавра, ФБИ-11, 3 курс
+    </footer>
+</body>
+</html>
+'''
